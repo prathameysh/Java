73694f5b4940fcdf219b0473aef53d359d4fdcf1
prathameysh <prathameshgavatre@gmail.com>
2025-04-30 17:07:21 +0530

    adjust bracket

--- code.java
+++ code.java
@@ -1,5 +1,7 @@
-public class code {
-    public static void main(String[] args) {
+public class code 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("Hello, Java in VS Code!");
     }
 }
