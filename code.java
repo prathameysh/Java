bc27727e2af58673a62b2742a5d9abe05f25917e
prathameysh <prathameshgavatre@gmail.com>
2025-05-02 18:39:59 +0530

    experimenting

--- code.java
+++
@@ -1,9 +0,0 @@
-class code {
-    public static void main(String[] args) {
-
-        int num;
-
-        System.out.println("Hello, Java in VS Code!");
-    }
-}
-    
---
+++ codetwo.java
@@ -0,0 +1,18 @@
+class code {
+    public static void main(String[] args) {
+
+        int num;
+
+        System.out.println("Hello, Java in VS Code! one");
+    }
+}
+
+class codetwo {
+    public static void main(String[] args) {
+
+        int num;
+
+        System.out.println("Hello, Java in VS Code! two");
+    }
+}
+    
